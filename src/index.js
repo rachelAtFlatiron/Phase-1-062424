1d4832ae2ed08c4fc27778539fce4d4eacbec01e
Rachel Katz <rachel.katz@flatironschool.com>
2024-03-01 12:01:19 -0500

    demo done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,65 @@ const resultsDiv = document.querySelector("#results");
 const apiSearchForm = document.querySelector("#search-form");
 const apiRoot = "https://api.tvmaze.com";
 
+
+const renderShow = (episode) => {
+    //create elements
+    const title = document.createElement('h2')
+    const season = document.createElement('p')
+    const image = document.createElement('img')
+    const summary = document.createElement('p')
+
+    //update elements with values
+    title.textContent = episode.name
+    season.textContent = `Season ${episode.season} Episode ${episode.number}`
+    image.src = episode.image.medium 
+    summary.textContent = episode.summary
+
+    //append to results div
+    resultsDiv.append(title)
+    resultsDiv.append(season)
+    resultsDiv.append(image)
+    resultsDiv.append(summary)
+}
+
 //✅ 1. on tv maze api search form submit 
-//✅ 1a. send search value to API
+apiSearchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let query = e.target.search.value 
+    //✅ 1a. send search value to API
+    //colon represents that it expects query to be a variable
+    fetch(`${apiRoot}/singlesearch/shows?q=${query}`)
+    .then(res => {
+        if(res.ok){
+            return res.json()
+        } else {
+            console.error('something went wrong')
+        }
+    })
+    //someone can do a blog on Promises.all
+    .then(data => {
+        console.log(data.id)
+        fetch(`${apiRoot}/shows/${data.id}/episodes`)
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            } else {
+                console.error('soemthing went wrong')
+            }
+        })
+        .then(episodes => {
+            console.log(episodes)
+            episodes.forEach(show => renderShow(show))
+        })
+    })
 
-//✅ 1b. render title, image, summary of show
+    //✅ 1b. render title, image, summary of show
 
 
-//✅ 1c. render first episode of show
+    //✅ 1c. render first episode of show
+
+})
+
 
 
 //✅ 1d. render all episodes of show
